992340e7e09e5ec696333d9c40aa5ba464b0802d
Gromakov Daniil <danila.gromakov@gmail.com>
2024-04-23 22:53:58 +0300

    2 Дз

--- store/quality_control/admin.py
+++ store/quality_control/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import BugReport
+from .models import FeatureRequest
 
 # Register your models here.
+class BugReportAdminInline(admin.TabularInline):
+    model = BugReport
+    extra = 0
+    list_display = ('title','project', 'status', 'priority', 'created_at')
+    list_filter = ('title', 'status')
+    search_fields = ('title', 'status')
+    ordering = ('created_at',)
+    list_editable = ('status',)
+    readonly_fields = ('created_at',)
+    can_delete = True
+    show_change_link = True
+
+@admin.register(BugReport)
+class BugReportAdmin(admin.ModelAdmin):
+    list_display = ('title','project', 'status', 'priority', 'created_at')
+    list_filter = ('title', 'status')
+    search_fields = ('title', 'status')
+    ordering = ('created_at',)
+    list_editable = ('status',)
+    readonly_fields = ('created_at',)
+
+
+@admin.register(FeatureRequest)
+class FeatureRequestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'priority', 'created_at')
+    list_filter = ('title', 'status')
+    search_fields = ('title', 'status')
+    ordering = ('created_at',)
+    list_editable = ('status',)
+    readonly_fields = ('created_at',)
